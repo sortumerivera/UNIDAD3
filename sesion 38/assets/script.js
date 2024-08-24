820bc7c260c12693d64242e46901b8d5520d53ba
sortumerivera <sorayakiaratumerivera@gmail.com>
2024-08-23 21:16:49 -0500

    first commit

---
+++ sesion 38/assets/script.js
@@ -0,0 +1,77 @@
+
+/*let numsImpares = [1, 3, 5, 7, 9];
+console.log("Recorrer el array por indices");
+for (let i = 0; i < numsImpares.length; i++) {
+    console.log(numsImpares[i]);
+}
+
+console.log("");
+
+console.log("Recorrer el array al reves");
+for (let i = numsImpares.length - 1; i >= 0; i--) {
+    console.log(numsImpares[i]);
+}
+
+console.log("");
+
+console.log("Recorrer el array por elementos");
+for (let num of numsImpares) {
+    console.log(num);
+}
+
+
+ nums = [];
+console.log(nums);
+
+nums[0] = 10;
+nums[1] = 20;
+nums[2] = 30;
+nums[3] = 40;
+nums[4] = 50;
+
+console.log(nums);
+
+nums[2] = 8693;
+console.log(nums);
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+ matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+
+console.log(nums);
+console.log(nums.length);
+console.log("");
+
+console.log(matriz);
+console.log(matriz.length);
+console.log("");
+
+console.log(matriz[0]);
+console.log(matriz[1]);
+console.log(matriz[2]);
+
+console.log(matriz[0][0]);
+console.log(matriz[0][2]);
+console.log(matriz[1][1]);
+console.log(matriz[2][1]);
+*/
+
+let nums = [1, 2, 3, 4, 5];
+console.log(nums);
+
+nums.push(6);
+nums.push(7);
+nums.push("HOLAAAA");
+console.log(nums);
+
+let elementoEliminado = nums.pop();
+console.log(nums);
+
+console.log(elementoEliminado);
+nums.pop();
+nums.pop();
+console.log(nums);
